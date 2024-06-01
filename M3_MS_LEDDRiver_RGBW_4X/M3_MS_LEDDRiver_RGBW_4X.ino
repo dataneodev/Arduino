b2078b37b2d569be54f080b5c87f7a8f0610cd1c
dataneo <BPrv6QazVbwni@gmail.com>
2024-06-01 22:15:37 +0200

    [M3_MS_LEDDRiver_RGBW_4X]

--- M3_MS_LEDDRiver_RGBW_4X/M3_MS_LEDDRiver_RGBW_4X.ino
+++ M3_MS_LEDDRiver_RGBW_4X/M3_MS_LEDDRiver_RGBW_4X.ino
@@ -164,16 +164,44 @@ uint8_t channel4Level = DEFAULT_CH4;
 //unsigned long lastMessageRevice;
 /* #endregion */
 
-
-
-
 void before() {
+  ///cpu_slow_speed();
   Serial2.begin(9600);
   inicjalizePins();
   inicjalizeI2C();
   readSettingFromEprom();
 }
 
+extern "C" void SystemClock_Config(void)
+{
+ RCC_OscInitTypeDef RCC_OscInitStruct = {};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+    Error_Handler();
+  }
+}
 
 void setup() {
 
