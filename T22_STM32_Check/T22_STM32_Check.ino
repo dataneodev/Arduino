5eb49d112fb24fe925995e83f4e78c2cf1cc472c
dataneo <BPrv6QazVbwni@gmail.com>
2024-06-11 23:57:59 +0200

    [T22_STM32]

--- T22_STM32_Check/T22_STM32_Check.ino
+++ T22_STM32_Check/T22_STM32_Check.ino
@@ -1,37 +1,53 @@
-#define ARDUINO_ARCH_STM32F1
 
-//                      RX    TX
-HardwareSerial Serial2(PA10, PA9);
-
-#define MY_DISABLED_SERIAL         // manual configure Serial1
-#define MY_RS485                   // Enable RS485 transport layer
-#define MY_RS485_DE_PIN PA1         // Define this to enables DE-pin management on defined pin
-#define MY_RS485_BAUD_RATE 9600    // Set RS485 baud rate to use
-#define MY_RS485_HWSERIAL Serial2  //
-#define MY_RS485_SOH_COUNT 6
-#define MY_TRANSPORT_WAIT_READY_MS 1
-
-#include <MySensors.h>
-#include "24C32.h"
-#include <Wire.h>
-
-
-
-
-
-TwoWire Wire2(PB11, PB10);
-#define Wire Wire2
-
-EE EEPROM24C32;
-
+extern "C" void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+ 
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_LSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV2;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+ 
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+ 
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC|RCC_PERIPHCLK_USB;
+  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
 
 void setup() {
-  Wire2.begin(); 
-  EEPROM24C32.begin(0x50, false);
 
   pinMode(PC13, OUTPUT);
-  digitalWrite(PC13, EEPROM24C32.checkPresence());
-
+  
+digitalWrite(PC13, HIGH);
 
   // put your setup code here, to run once:
 
@@ -39,5 +55,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+digitalWrite(PC13, HIGH);
 }
